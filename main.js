da5c91943c9b3dfd6f619d8097f5c4723d6bb311
the-mand0 <nooritimor2+new@gmail.com>
2024-12-07 10:13:39 +0300

    programmed the calculator logic it only works with adding but I will make the other operations functional as well

--- main.js
+++ main.js
@@ -1,27 +1,31 @@
-const result = document.querySelector("#result");
+const output = document.querySelector("#result");
 const numBtns = document.querySelectorAll(".number");
+const operationBtns = document.querySelectorAll(".operation");
 
-let firsNum;
-let secondNum;
+let operations = [];
 
-function add(){
-    return firsNum + secondNum;
-}
 
-function subtract(){
-    return firsNum - secondNum;
-}
-
-function multiply(){
-    return firsNum * secondNum;
-}
-
-function divide(){
-    return firsNum / secondNum;
+function operate() {
+    if (operations.length === 1) {
+        result = operations[0]['value'];
+    } else {
+        operations[1]['value'] = operations[0]['value'] + operations[1]['value'];
+        operations.shift();
+    }
 }
 
 numBtns.forEach((button) => {
     button.addEventListener("click", () => {
-        result.value = result.value + button.innerText;
+        output.value = output.value + button.innerText;
+    })
+});
+
+operationBtns.forEach((button) => {
+    button.addEventListener('click', () => {
+        operations.push({'value': Number(output.value), 'operationType': button.id});
+        operate();
+        output.value = '';
+        if (button.id === 'equals') output.value = operations[0]['value'];
+        console.log(operations);
     })
 });
